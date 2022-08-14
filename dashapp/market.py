f32c60ac6d773a7ec17d26ac5acbc3d48876a5b7
Karim Khalil <karim.khalil@protonmail.com>
2022-08-14 20:22:00 +0200

    dynamic datepicker and button

--- dashapp/market.py
+++ dashapp/market.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from datetime import date
 
 from dashapp.instance import dash_app
@@ -59,16 +59,27 @@ market = html.Div([
             html.Br(),
             html.Div(id='div_date', children=[]),
             dcc.Graph(id = 'market-idx-graph', figure={}),
-            dcc.DatePickerRange(
-                                id='idx-date-picker',
-                                min_date_allowed=date(1995, 8, 5),
-                                max_date_allowed=date(2017, 9, 19),
-                                initial_visible_month=date(2017, 8, 5),
-                                end_date=date(2017, 8, 25)
-                                ),
+            # dcc.DatePickerRange(
+            #                     id='idx-date-picker',
+            #                     min_date_allowed=date(1970, 1, 1),
+            #                     max_date_allowed=date(2022, 12, 30),
+            #                     initial_visible_month=date(2022, 12, 30),
+            #                     end_date=date(2022, 12, 30)
+            #                     ),
+            html.H4(id='store-out'),
             dcc.Store(id='memory-test', data=[], storage_type='memory')
 ])
 
+html_date  = dbc.Row([dbc.Col(dcc.DatePickerRange(
+                            id='idx-date-picker2',
+                            min_date_allowed=date(1970, 1, 1),
+                            max_date_allowed=date(2022, 12, 30),
+                            initial_visible_month=date(2022, 12, 30),
+                            end_date=date(2022, 12, 30)
+                            )),
+                    dbc.Col(dbc.Button('Submit'))
+                    ],
+                    )
 
 @dash_app.callback(
     [Output(component_id='market-idx-graph', component_property='figure'),
@@ -76,7 +87,7 @@ market = html.Div([
     Output(component_id='div_date', component_property='children'),
     Output(component_id='memory-test', component_property='data')],
     [Input(component_id='market-idx-dropdown', component_property='value')],
-    prevent_initial_call=True
+    prevent_initial_call=True, preventupdate=True
 )
 
 def drop_down(option):
@@ -96,16 +107,31 @@ def drop_down(option):
 
     print(f'selection is {option} from {date_min_str} to {date_max_str}\n')
     fig = px.line(df_fig, x='datetime', y = 'adjClose', color='name')
-    html_date  = dcc.DatePickerRange(
-                                id='idx-date-picker2',
-                                min_date_allowed=date_min,
-                                max_date_allowed=date_max,
-                                initial_visible_month=date_min,
-                                end_date=date_max
-                                )
+
+
 
     print(type(dict_fig))
     return fig ,option, html_date, dict_fig
+
+@dash_app.callback(
+    [Output(component_id='store-out', component_property='children')],
+    [Input(component_id='idx-date-picker2', component_property='start_date'),
+    Input(component_id='idx-date-picker2', component_property='end_date')],
+    prevent_initial_call=True)
+
+def store(start_date, end_date):
+    if start_date is not None:
+        start = start_date
+    if end_date is not None:
+        end = end_date
+
+    print(start, end)
+    # df = pd.DataFrame(data)
+    # shape = df.shape
+    output = [f'columns found- start {start} {end}']
+    print(output)
+    return output
+
 
 if __name__ == '__main__':
     fig.show()
